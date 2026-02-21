c6cca94d76fb517f5f04271e3236333f6be94b31
Rupakop <onev523@gmail.com>
2026-02-21 20:49:05 +0530

    Add about.php with foundation information

---
+++ about.php
@@ -0,0 +1,6 @@
+<?php include 'includes/header.php'; ?>
+
+<h2>About Us</h2>
+<p>Satyarthy Foundation works for community upliftment through education and healthcare initiatives.</p>
+
+<?php include 'includes/footer.php'; ?>
