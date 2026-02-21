a416d46e7b27767ab050433b4529adff3d2f4bb2
Rupakop <onev523@gmail.com>
2026-02-21 20:56:40 +0530

    Add header.php for site navigation and title

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Satyarthy Foundation</title>
+    <link rel="stylesheet" href="/assets/style.css">
+</head>
+<body>
+
+<nav>
+    <a href="/index.php">Home</a>
+    <a href="/about.php">About</a>
+    <a href="/projects.php">Projects</a>
+    <a href="/contact.php">Contact</a>
+    <a href="/volunteer.php">Volunteer</a>
+</nav>
+<hr>
