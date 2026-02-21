61a712b3f7e52c86b1d275b01a2d819751521c47
Rupakop <onev523@gmail.com>
2026-02-21 20:47:46 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include 'includes/header.php'; ?>
+
+<h1>Welcome to Satyarthy Foundation</h1>
+<p>We support education, healthcare and social welfare initiatives.</p>
+
+<?php include 'includes/footer.php'; ?>
