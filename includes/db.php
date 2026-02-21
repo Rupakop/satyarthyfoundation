17fc0d270c56dd4e044369cf1b71f5a73848f221
Rupakop <onev523@gmail.com>
2026-02-21 20:55:27 +0530

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,1 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "satyarthy_db";
 
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
